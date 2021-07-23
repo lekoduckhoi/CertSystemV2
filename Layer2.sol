c23b23aca163a1834e0df004a04f66eabcaf0497
lekoduckhoi <63868028+lekoduckhoi@users.noreply.github.com>
2021-07-23 19:37:12 +0700

    Update Layer2.sol

--- Layer2.sol
+++ Layer2.sol
@@ -8,27 +8,72 @@ contract Organization {
     uint public orgId;
     string public orgName;
     string public orgLink;
+    uint public creationTime;
     uint public activityCount;
     
+    uint totalAddedActivity; //include removed activities
+    
+    event UpdateInfo(uint OrgId, string OrgName, string Orglink);
+    
+    function updateInfo(uint _orgId, string memory _orgName, string memory _orglink) public {
+        require(msg.sender == owner, "must be owner");
+        orgId = _orgId;
+        orgName = _orgName;
+        orgLink = _orglink;
+        emit UpdateInfo(_orgId, _orgName, _orglink);
+    }
+    
     //owner
-    address owner;
+    address public owner;
     function setNewOwner(address newOwnerAddress) public {
         require(msg.sender == owner, 'must be owner');
         owner = newOwnerAddress;
     }
     
+    //constructor
+    constructor(uint _orgId, string memory _orgName, string memory _orglink) {
+        orgId = _orgId;
+        orgName = _orgName;
+        orgLink = _orglink;
+        owner = tx.origin;
+        creationTime = block.timestamp;
+    }
+    
     //Acitivitys
-    struct Acitivity {
+    event AddActivity(Activity);
+    event RemoveActivity(Activity);
+    
+    mapping(uint => Activity) public acitivityById;
+    
+    address[] public allActivityAdresses;
+    
+    struct Activity {
         uint id;
         string name;
         address acitivityContractAddress;
     }
     
+    function viewAllActivityAddress() public view returns(address[] memory) {
+        return allActivityAdresses;
+    }
+    
     function addNewActivity(string memory _activityName, string memory _period, string memory _link) public {
         require(msg.sender == owner, "must be owner");
+        ActivityContract newActivty = new ActivityContract(totalAddedActivity, _activityName, orgName, _period, _link);
+        acitivityById[activityCount] = Activity(totalAddedActivity, _activityName, address(newActivty));
+        allActivityAdresses.push(address(newActivty));
         activityCount++;
-        Activity newActivty = new Activity(_activityName, orgName, _period, _link);
-        
+        totalAddedActivity++;
+        emit AddActivity(Activity(totalAddedActivity, _activityName, address(newActivty)));
     }
+    
+    function removeActivityById(uint _id) public {
+        require(msg.sender == owner, "must be owner");
+        activityCount--;
+        delete allActivityAdresses[_id];
+        delete acitivityById[_id];
+        emit RemoveActivity(acitivityById[_id]);
+    }
+    
     
 }
