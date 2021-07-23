29984c26f353ed5b6c431c55931188e1052c6b3e
lekoduckhoi <63868028+lekoduckhoi@users.noreply.github.com>
2021-07-24 02:00:39 +0700

    Update Layer1.sol

--- Layer1.sol
+++ Layer1.sol
@@ -6,16 +6,54 @@ contract CertSystemLayer1 {
     
     string public systemName = "Blockchain-based Certificate System";
     address public creator;
+    uint public orgCount;
     
-    uint public package50Price;
-    uint public package150Price;
-    uint public packageUnlimitedPrice;
+    // 3 options: 50/150/unlimited Certificate Activity   PackagePrice[0] = pack50Price; PackagePrice[1] = pack150Price , PackagePrice[2] = packunlimitedPrice;
+    uint[3] public PackagePrice;
     
-    function updatePrice(uint _package50Price, uint _package150Price, uint _packageUnlimitedPrice) public {
+    function updatePrice(uint _pack50Price, uint _pack150Price, uint _packUnlimitedPrice) public {
         require(msg.sender == creator, 'must be creator');
-        package50Price = _package50Price;
-        package150Price = _package150Price;
-        packageUnlimitedPrice = _packageUnlimitedPrice;
+        PackagePrice = [_pack50Price, _pack150Price, _packUnlimitedPrice];
+    }
+    
+    //Organization
+    event Register(Organization);
+    event UpdatePackage(uint OrgId, uint UpdatedPaceType);
+    
+    Organization[] allOrganizations;
+    
+    mapping(uint => uint[3]) public totalPackById;       //org id 1 => [totalPack50, totalPack150, totalPackUnlimited];
+    
+    struct Organization {
+        uint orgId;
+        string orgName;
+        address orgOwner;
+        address orgContractAddress;
+    }
+    
+    function viewAllOrgAddress() public view returns(Organization[] memory) {
+        return allOrganizations;
+    }
+    function viewTotalPackById(uint _id) public view returns(uint[3] memory) {
+        return totalPackById[_id];
+    }
+    
+    function register(string memory _orgName, string memory _orglink, uint firstPackType) public payable {   // firstPackType can be 0,1,2 => 50,150,unlimited
+        require(firstPackType < 3, "package does not exists");
+        require(msg.value == PackagePrice[firstPackType], "wrong value");
+        OrganizationContract newOrganization = new OrganizationContract(orgCount, _orgName, _orglink);
+        allOrganizations.push(Organization(orgCount, _orgName, msg.sender, address(newOrganization)));
+        totalPackById[orgCount][firstPackType]++;
+        orgCount++;
+        emit Register(Organization(orgCount, _orgName, msg.sender, address(newOrganization)));
+    }   
+    
+    function updatePackage(uint _orgId, uint addPackType) public payable { 
+        require(msg.sender == allOrganizations[_orgId].orgOwner);
+        require(addPackType < 3, "wrong pack type");
+        require(msg.value == PackagePrice[addPackType], "wrong value");
+        totalPackById[_orgId][addPackType]++;
+        emit UpdatePackage(_orgId, addPackType);
     }
     
     
